a5e27a7bf108f6b19e02f160e39f1f6c6e700302
BintangRF <bintangrama777@gmail.com>
2023-09-09 16:56:47 +0700

    menambah poto profil

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const app = express();
+const multer = require("multer");
+const fs = require("fs");
 
 // Konfigurasi koneksi MySQL
 const db = mysql.createConnection({
@@ -38,6 +40,27 @@ const checkLoggedIn = (req, res, next) => {
   }
 };
 
+// poto-profil
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Simpan gambar di folder "uploads/"
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname); // Nama file unik
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // Batas ukuran file (misalnya, 5MB)
+  },
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); // Folder untuk file publik (CSS, gambar, dll.)
+
 // Routing
 app.get("/", (req, res) => {
   res.redirect("/dashboard");
@@ -57,18 +80,28 @@ app.get("/signup", (req, res) => {
 app.get("/dashboard", (req, res) => {
   if (req.session.userId) {
     const userId = req.session.userId;
-    const query = "SELECT nama_pasien FROM tb_pasien WHERE id_pasien = ?";
+    const query =
+      "SELECT nama_pasien, foto_pasien FROM tb_pasien WHERE id_pasien = ?";
     db.query(query, [userId], (err, results) => {
       if (err) throw err;
       if (results.length === 1) {
-        const nama_pasien = results[0].nama_pasien;
-        res.render("dashboard.ejs", { nama: nama_pasien }); // Ubah nama_pasien menjadi nama
+        const { nama_pasien, foto_pasien } = results[0];
+        const id_pasien = userId;
+        res.render("dashboard.ejs", {
+          nama: nama_pasien,
+          id_pasien: id_pasien,
+          foto_pasien: foto_pasien, // Menambahkan foto_pasien ke konteks template
+        });
       } else {
         res.redirect("/login");
       }
     });
   } else {
-    res.render("dashboard.ejs", { nama: null });
+    res.render("dashboard.ejs", {
+      nama: null,
+      id_pasien: null,
+      foto_pasien: null,
+    });
   }
 });
 
@@ -121,6 +154,50 @@ app.get("/appointment", (req, res) => {
   }
 });
 
+// Route untuk halaman profil
+app.get("/profile", checkLoggedIn, (req, res) => {
+  const id_pasien = req.session.userId;
+
+  // Query SQL untuk mengambil data pengguna dari tb_pasien
+  const query =
+    "SELECT foto_pasien, id_pasien, email_pasien, nama_pasien, alamat, nomor_ponsel FROM tb_pasien WHERE id_pasien = ?";
+
+  db.query(query, [id_pasien], (err, results) => {
+    if (err) {
+      throw err;
+    }
+
+    if (results.length === 1) {
+      const profileData = results[0];
+
+      // Render halaman profil dengan data pengguna
+      res.render("profile.ejs", { profileData });
+    }
+  });
+});
+
+// Edit Profile
+app.get("/edit_profile", checkLoggedIn, (req, res) => {
+  const id_pasien = req.session.userId;
+
+  // Query SQL untuk mengambil data pengguna dari tb_pasien
+  const query =
+    "SELECT id_pasien, email_pasien, nama_pasien, alamat, nomor_ponsel FROM tb_pasien WHERE id_pasien = ?";
+
+  db.query(query, [id_pasien], (err, results) => {
+    if (err) {
+      throw err;
+    }
+
+    if (results.length === 1) {
+      const profileData = results[0];
+
+      // Render halaman edit profil dengan data pengguna
+      res.render("edit_profile.ejs", { profileData });
+    }
+  });
+});
+
 // Route Logout
 app.get("/logout", (req, res) => {
   req.session.destroy((err) => {
@@ -137,7 +214,8 @@ app.listen(port, () => {
 });
 
 // SIGNUP
-app.post("/signup", (req, res) => {
+
+app.post("/signup", upload.single("foto_pasien"), (req, res) => {
   const {
     nama_pasien,
     tanggal_lahir,
@@ -148,8 +226,20 @@ app.post("/signup", (req, res) => {
     password,
   } = req.body;
 
-  // Simpan data pengguna ke database
+  // Ambil file gambar yang diunggah
+  const foto_pasien = req.file;
+
+  // Pastikan file gambar telah diunggah dengan benar
+  if (!foto_pasien) {
+    return res.status(400).send("Gambar profil harus diunggah.");
+  }
+
+  // Konversi gambar menjadi bentuk Base64
+  const gambarBase64 = fs.readFileSync(foto_pasien.path, "base64");
+
+  // Simpan data pengguna ke database, termasuk gambar profil
   const user = {
+    foto_pasien: gambarBase64, // Simpan gambar dalam format Base64
     nama_pasien,
     tanggal_lahir,
     gender,
@@ -162,7 +252,7 @@ app.post("/signup", (req, res) => {
   const query = "INSERT INTO tb_pasien SET ?";
   db.query(query, user, (err, results) => {
     if (err) {
-      throw err;
+      return res.status(500).send("Pendaftaran gagal. Coba lagi nanti.");
     }
 
     // Tampilkan pesan sukses dan arahkan pengguna ke halaman login
@@ -178,7 +268,6 @@ app.post("/signup", (req, res) => {
 
 //
 
-// LOGIN
 // LOGIN
 app.post("/login", (req, res) => {
   const { email_pasien, password } = req.body;
@@ -208,6 +297,55 @@ app.post("/login", (req, res) => {
     }
   });
 });
+
+//
+
+// Update Profil
+app.post(
+  "/edit_profile",
+  checkLoggedIn,
+  upload.single("foto_pasien"),
+  (req, res) => {
+    const id_pasien = req.session.userId;
+    const { nama_pasien, email_pasien, alamat, nomor_ponsel } = req.body;
+
+    // Lakukan validasi dan pembaruan data profil di sini sesuai kebutuhan
+    // Misalnya, Anda dapat melakukan validasi data yang diterima dan memastikan data tersebut valid
+
+    // Jika foto profil berhasil diunggah, Anda dapat menghandle pembaruan gambar profil di sini
+    if (req.file) {
+      const newProfilePicture = req.file.filename;
+      // Lakukan sesuatu dengan gambar profil yang baru diunggah, jika diperlukan
+    }
+
+    // Query SQL untuk melakukan pembaruan data profil di tabel tb_pasien
+    const updateQuery =
+      "UPDATE tb_pasien SET nama_pasien = ?, email_pasien = ?, alamat = ?, nomor_ponsel = ? WHERE id_pasien = ?";
+
+    db.query(
+      updateQuery,
+      [nama_pasien, email_pasien, alamat, nomor_ponsel, id_pasien],
+      (err, result) => {
+        if (err) {
+          // Handle kesalahan jika query gagal
+          console.error("Kesalahan saat memperbarui profil:", err);
+          return res
+            .status(500)
+            .send("Terjadi kesalahan saat memperbarui profil.");
+        }
+
+        // Setelah pembaruan berhasil, tampilkan pesan sukses dan arahkan ke halaman profil
+        const successMessage = "Profil berhasil diperbarui";
+        res.send(`
+        <script>
+          alert('${successMessage}');
+          window.location='/profile'; // Redirect ke halaman profil
+        </script>
+      `);
+      }
+    );
+  }
+);
 
 //
 
